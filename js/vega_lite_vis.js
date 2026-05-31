c6c2ec58b0e4a85f875435995f6854a8ce64d203
amoh0228 <amoh0228@student.monash.edu>
2026-05-31 20:57:29 +0800

    creating folder and organizing the files

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,29 @@
+vegaEmbed('#vis1', 'js/chart1.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis2', 'js/chart2.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis3', 'js/chart3.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis4', 'js/chart4.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis5', 'js/chart5.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis6', 'js/chart6.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis7', 'js/chart7.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis8', 'js/chart8.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis9', 'js/chart9.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
+
+vegaEmbed('#vis10', 'js/chart10.json', { "actions": false }).then(function (result) {
+    }).catch(console.error);
